ff307ed9f3e06bdb7b54d1ecb13a0cebbf323735
abiodun <114649936+kaemoney0147@users.noreply.github.com>
2023-03-29 14:46:31 +0100

    Update AllPatient.jsx

--- src/component/allpatient/AllPatient.jsx
+++ src/component/allpatient/AllPatient.jsx
@@ -28,8 +28,8 @@ export default function AllPatient() {
 
   function getDaysDiff(date1, date2) {
     const oneDay = 12 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
-    const newPatientDays = Math.round(Math.abs((date1 - date2) / oneDay));
-    return newPatientDays;
+    const diffDays = Math.round(Math.abs((date1 - date2) / oneDay));
+    return diffDays;
   }
 
   const [query, setQuery] = useState("");
@@ -131,7 +131,7 @@ export default function AllPatient() {
             .map((p, index) => (
               <Col className="allPatientCol col-4 mb-3" key={index}>
                 <Card id="patientlistCard">
-                  {setNewPatientDays(new Date(p.createdAt), new Date()) <=
+                  {getDaysDiff(new Date(p.createdAt), new Date()) <=
                     newPatientDays && (
                     <Badge variant="danger" className="new-patient-badge">
                       New Admission
